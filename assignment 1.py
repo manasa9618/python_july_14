8c97cff4290b5118c29037195b2deb02d660cda1
manasa9618 <87527050+manasa9618@users.noreply.github.com>
2021-07-24 17:43:11 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,90 @@
+#conversions
+#string conversions
+
+#string to int
+a='60'
+print(type(a), a)
+b=int(a)
+print(type(b), b)
+
+
+#string to float
+a='78'
+print(type(a), a)
+b=float(a)
+print(type(b), b)
+
+
+#string to bool
+a='50'
+print(type(a), a)
+b=bool(a)
+print(type(b), b)
+
+#float conversions
+#float to int
+
+a=2.3
+print(type(a), a)
+b=int(a)
+print(type(b), b)
+
+#float to string
+a=2.5
+print(type(a), a)
+b=str(a)
+print(type(b), b)
+
+#string to bool
+a=6.7
+print(type(a), a)
+b=bool(a)
+print(type(b), b)
+
+#int conversions
+#int to float
+a=60
+print(type(a), a)
+b=float(a)
+print(type(b), b)
+ 
+#int to string
+a=40
+print(type(a), a)
+b=str(a)
+print(type(b), b)
+
+#int to bool
+a=28
+print(type(a), a)
+b=bool(a)
+print(type(b), b)
+
+#bool conversions
+#bool to str
+a=True
+print(type(a), a)
+b=str(a)
+print(type(b), b)
+
+#bool to int
+a=True
+print(type(a), a)
+b=int(a)
+print(type(b), b)
+
+#bool to str
+a=True
+print(type(a), a)
+b=str(a)
+print(type(b), b)
+
+#bool to float
+a=False
+print(type(a), a)
+b=float(a)
+print(type(b), b)
+
+
+
+
---
+++ assignment2.py
@@ -0,0 +1,12 @@
+name=input("enter a name : ")
+a=int(input("enter the marks in telugu : "))
+c=int(input("enter the marks in english : "))
+d=int(input("enter the marks in maths : "))
+e=int(input("enter the marks in social : "))
+f=int(input("enter the marks in science : "))
+total=(a+c+d+e+f)
+print(total)
+percentage=total/500*100
+print(percentage)
+average=total/5
+print(average)
